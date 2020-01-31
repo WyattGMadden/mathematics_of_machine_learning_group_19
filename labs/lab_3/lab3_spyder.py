e270c8c50b6ba350a08c6610766f4dd89c748035
dncrwlye <dancrowley.e@gmail.com>
2020-01-31 15:21:26 -0700

    adding in face figures

--- labs/lab_3/lab3_spyder.py
+++ labs/lab_3/lab3_spyder.py
@@ -7,31 +7,51 @@ Created on Fri Jan 31 12:25:21 2020
 """
 
 import scipy.io as scipy 
-import scipy as scipy 
 
 import numpy as np
 from scipy.stats import norm
 
 
-mat = scipy.loadmat('/Users/dancrowley/Documents/machine_learning_zosso/lab3/cbcl.mat')
+mat = scipy.loadmat('/Users/dancrowley/Documents/machine_learning_zosso/lab_3_dc/cbcl.mat')
 X = mat["X"]
 X.shape
+
+import scipy as scipy 
+
 #principle components calculator 
 M=6
     
-def princomp(X,M)
+def princomp(X,M):
 
     Y = X - X.mean(axis=1, keepdims=True)
     cov = np.cov(Y)
 
-    lambda = scipy.sparse.linalg.eigsh(cov, k= M, which = 'LM')[0]
+    lam = scipy.sparse.linalg.eigsh(cov, k= M, which = 'LM')[0]
     W = scipy.sparse.linalg.eigsh(cov, k= M, which = 'LM')[1]
     mu = X.mean(axis=1, keepdims=True)
     #not really sure if that is what we are suppose to do for Z
-    Z = np.transpose(X).dot(W)
+    Z = np.transpose(W).dot(X)
 
 
-    return(W, Z, mu, lambda)
+    return(W, Z, mu, lam)
 
+
+
+returned = princomp(X, 2)
+
+import matplotlib.pyplot as plt
+
+
+def faceplot(X, i):
+    face = [row[i] for row in X]
+    face_mat = np.reshape(face, (19,19))
+    fig = plt.imshow(face_mat)
+    return(fig)
+    
+ faceplot(X, 10)
+ 
+ 
+ faceplot(returned[0][0:,], 0)
+ faceplot(returned[0][0:,], 1)
 
 
