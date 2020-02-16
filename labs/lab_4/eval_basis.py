faca123ebd43a809b7a6a9729c1cf5e8930c3499
Wyatt Madden <wyattmaddensonoma@gmail.com>
2020-02-15 20:09:26 -0700

    fixed .py functions

--- labs/lab_4/eval_basis.py
+++ labs/lab_4/eval_basis.py
@@ -8,9 +8,10 @@
 # Outputs:
 #  B - Matrix with the values of the basis functions at the locations in xeval.
 #    Each column of B corresponds to a basis function.
+import numpy
 
 def eval_basis(params, func, xeval):
-    B = zeros(length(xeval), size(params,2));
-    for j = 1:size(params,2)
-        B(:,j) = func(xeval, params(:,j));
+    B = numpy.zeros(length(xeval), numpy.size(params,2));
+    for j in range(1, size(params,2)):
+        B[:,j] = func(xeval, params[:,j]);
     return basis
