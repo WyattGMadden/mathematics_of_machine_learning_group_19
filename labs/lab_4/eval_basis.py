874c2c3fd8944c5fac39f93b889b92252dd55eac
dncrwlye <dancrowley.e@gmail.com>
2020-02-15 18:47:52 -0700

    i believe this works

--- labs/lab_4/eval_basis.py
+++ labs/lab_4/eval_basis.py
@@ -9,8 +9,12 @@
 #  B - Matrix with the values of the basis functions at the locations in xeval.
 #    Each column of B corresponds to a basis function.
 
-def eval_basis(params, func, xeval):
-    B = zeros(length(xeval), size(params,2));
-    for j = 1:size(params,2)
-        B(:,j) = func(xeval, params(:,j));
-    return basis
+import numpy as np
+
+def eval_basis(params, xeval):
+    len = xeval.shape[0]
+    B = np.zeros(shape = (params, len))
+    for j in range(0, params):
+        B[j] = np.power(xeval,j).reshape(len,)
+    return(B)
+
