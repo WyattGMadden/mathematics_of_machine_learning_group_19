425e702ee6d3749d3a081e2645923edc8520c36d
dncrwlye <dancrowley.e@gmail.com>
2020-02-29 17:46:28 -0700

    fixed some stuff

--- labs/bayes_phylo/first_attempt.R
+++ labs/bayes_phylo/first_attempt.R
@@ -1,10 +1,13 @@
 
 library(ape)
 library(phylofactor)
-set.seed(1)
+
+#set.seed(1)
 
 #create a tree with n tips
-n= 120
+
+n= rpois(1,120)
+#n=120
 tree <-rtree(n)
 
 #plot tree if you would like:
@@ -17,16 +20,18 @@ max_groups <- 2*n-2
 
 #identify the two clades we would like
 
-clade1 = getPhyloGroups(tree)[104][1][[1]][[1]]
-clade2 = getPhyloGroups(tree)[128][1][[1]][[1]]
+#clade1 = getPhyloGroups(tree)[104][1][[1]][[1]]
+#clade2 = getPhyloGroups(tree)[128][1][[1]][[1]]
 
-#clade1 <- phangorn::Descendants(tree,128,'tips')[[1]]
-#clade2 <- phangorn::Descendants(tree,186,'tips')[[1]]
-#clade3 <- phangorn::Descendants(tree,131,'tips')[[1]]
+clade1 = getPhyloGroups(tree)[sample(max_groups, 1)][1][[1]][[1]]
+clade2 = getPhyloGroups(tree)[sample(max_groups, 1)][1][[1]][[1]]
+
+if (length(clade1) < 2 ) {next}
+if (length(clade2) < 2 ) {next}
 
 #randomly generate data, then increase size of clade 1 and decrease clade 2                                               
-set.seed(1)
-BodySize <-rlnorm(100)
+
+BodySize <-rlnorm(n)
 BodySize[clade1] <-rlnorm(length(clade1))*4
 BodySize[clade2] <-rlnorm(length(clade2))/4
 
@@ -35,7 +40,7 @@ BodySize = as.data.frame(cbind(BodySize, tree$tip.label, basis = 1))
 
 #now we generate the missing data.
 #these are species we haven't observed data for
-num_miss = 20
+num_miss = rpois(1,80)
 miss_tip = sample(tree$tip.label, num_miss)
 
 #first drop tips from tree
@@ -66,10 +71,11 @@ for (i in 1: (2*n-2))
   #product of two groups, missing observations
   
   product[i] = (0.0001 + sum(is.na(BodySize[grp1,]$BodySize_miss))/length(is.na(BodySize[grp1,]$BodySize)) * sum(is.na(BodySize[grp2,]$BodySize_miss))/length(is.na(BodySize[grp2,]$BodySize)))
-
+  #product[i] =  product[i]
+  product[i] =  sqrt(unlist(product[i]))
+  
   #create constrast basis 
   BodySize$basis_miss = c(as.numeric(BodySize[grp2,]$basis_miss)*-1, as.numeric(BodySize[grp1,]$basis_miss)*1)
-  
   
   #Bayesian Posterior Estimate
   S_o = product[i][[1]]
@@ -97,44 +103,61 @@ for (i in 1: (2*n-2))
 }
 
 which.max(results[,3]/results[,4])
-# 64
 getPhyloGroups(tree)[64]
 which.max(results[,1]/results[,2])
-# 64 also
 getPhyloGroups(tree)[64]
 
-plot(results[,1]/results[,2], results[,3]/results[,4])
+#plot(results[,1]/results[,2], results[,3]/results[,4])
 
-pf_fisher <-twoSampleFactor(log(BodySize$BodySize),tree,nfactors=2,method = "Fisher",ncores = 2)
+#pf_fisher <-twoSampleFactor(log(BodySize$BodySize),tree,nfactors=2,method = "Fisher",ncores = 2)
+#pf_fisher$groups
+#getPhyloGroups(tree)[2]
 
-clade1
-clade2
-pf_fisher$groups
-getPhyloGroups(tree)[2]
+#ggtree::ggtree(tree) +
+#   ggtree::geom_hilight(clade1)
 
-ggtree::ggtree(tree) +
-   ggtree::geom_hilight(clade1)
+#phangorn::Descendants(tree,104,'tips')[[1]]
 
-phangorn::Descendants(tree,104,'tips')[[1]]
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
+#..........................................cross validating................................................
 
 #how to make predictions for new clades 
 
-group = which.max(results[,3]/results[,4])
-grp1 = getPhyloGroups(tree)[group][[1]][[1]]
-grp2 = getPhyloGroups(tree)[group][[1]][[2]]
+group_map = which.max(results[,3]/results[,4])
+grp1 = getPhyloGroups(tree)[group_map][[1]][[1]]
+grp2 = getPhyloGroups(tree)[group_map][[1]][[2]]
 
-#create the predictions based on mu 
-
+#create the predictions based on mu_map
 #create a new variable, and label the boddy size and basis functions NA for these tips
 BodySize$BodySize_test = BodySize$BodySize
 BodySize$basis_test = BodySize$basis
-
 BodySize[!(BodySize$V2 %in% miss_tip),]$BodySize_test = NA
 BodySize[!(BodySize$V2 %in% miss_tip),]$basis_test = NA
+BodySize$basis_test = c(as.numeric(BodySize[grp2,]$basis_test)*-1, as.numeric(BodySize[grp1,]$basis_test)*1)
+#error group 1
+map_error = 1/n*(BodySize$basis_test * results[group_map,3] - BodySize$BodySize_test)^2 %>% sum(na.rm=TRUE)
+#error group 2
 
+group_mle = which.max(results[,1]/results[,2])
+grp1 = getPhyloGroups(tree)[group_mle][[1]][[1]]
+grp2 = getPhyloGroups(tree)[group_mle][[1]][[2]]
 
+#create the predictions based on mu_map
+#create a new variable, and label the boddy size and basis functions NA for these tips
+BodySize$BodySize_test = BodySize$BodySize
+BodySize$basis_test = BodySize$basis
+BodySize[!(BodySize$V2 %in% miss_tip),]$BodySize_test = NA
+BodySize[!(BodySize$V2 %in% miss_tip),]$basis_test = NA
 BodySize$basis_test = c(as.numeric(BodySize[grp2,]$basis_test)*-1, as.numeric(BodySize[grp1,]$basis_test)*1)
 
-(BodySize$basis_test * results[group,3] - BodySize$BodySize_test)^2 %>% sum(na.rm=TRUE)
-(BodySize$basis_test * results[group,1] - BodySize$BodySize_test)^2 %>% sum(na.rm=TRUE)
+mle_error = 1/n*(BodySize$basis_test * results[group_mle,1] - BodySize$BodySize_test)^2 %>% sum(na.rm=TRUE)
 
+chance_error = 1/n*(mean(BodySize$BodySize_test, na.rm =TRUE) - BodySize$BodySize_test)^2 %>% sum(na.rm=TRUE)
