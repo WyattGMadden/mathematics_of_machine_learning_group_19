068840eaab35c5f77a06e1e76f2089550d965251
dncrwlye <dancrowley.e@gmail.com>
2020-03-05 10:14:01 -0700

    fixed issue with covariance matrix

--- labs/bayes_phylo/gibbs_sampler.R
+++ labs/bayes_phylo/gibbs_sampler.R
@@ -10,21 +10,30 @@
 
 #### 1. Gibbs Sampler
 
-mat
+#mat
 
 set.seed(10262018)
-y = as.numeric(design.mat$BodySize_miss)
+y = as.numeric(design.mat[,"body_size_miss"])
 X=mat
 
 #X <- model.matrix(winpercent ~ chocolate_factor + nut_factor, data = candy)
-n = ncol(X)
 p <- ncol(X)
 n <- sum(X[,1])
 
 # Initialization and Prior
 num_mcmc <- 5000
 beta_0 <- rep(0,p)
-Sigma_0 <- diag(p) * product[i][[1]]
+
+#in Sigma_0, as the covariance gets larger, the estimate approaches the MLE 
+#we would like the opposite, as the product gets larger, we would like the estimate to approach 0
+#so, lets take the inverse of the product here
+#but let the covariance on the intercept be large, let that float 
+Sigma_0 <- diag(p) * c(1000, 1/product[i][[1]])
+#Sigma_0 <- diag(p) * c(1000, 1/1000)
+
+#we want: product is large, MAP approaches 0 
+#small product, map approaches the MLE 
+
 Sigma_0_inv <- solve(Sigma_0)
 nu_0 <- .02
 sigmasq_0 <- 1
@@ -34,22 +43,25 @@ sigmasq_samples <- rep(1, num_mcmc)
 for (iter in 2:num_mcmc){
   # sample beta
   cov_beta <- solve(Sigma_0_inv + t(X) %*% X / sigmasq_samples[iter - 1])
-  exp_beta <- cov_beta %*% (Sigma_0_inv %*% beta_0 + t(X) %*% y / sigmasq_samples[iter-1])
-  beta_samples[iter,] <- rmnorm(1, exp_beta, cov_beta) 
+  #exp_beta <- cov_beta %*% (Sigma_0_inv %*% beta_0 + t(X) %*% y / sigmasq_samples[iter-1])
+  exp_beta <- cov_beta %*% (t(X) %*% y / sigmasq_samples[iter-1])
+  
+  beta_samples[iter,] <- mnormt::rmnorm(1, exp_beta, cov_beta) 
   
   # sample sigmasq
-  sigmasq_samples[iter] <- rigamma(1, .5 * (nu_0 + n) , 
+  sigmasq_samples[iter] <- LearnBayes::rigamma(1, .5 * (nu_0 + n) , 
                                    .5 * (nu_0 * sigmasq_0 + t(y - X %*% beta_samples[iter,]) %*% 
                                            (y - X %*% beta_samples[iter,])) )
 }
 
-burn_in <- 100
-beta_samples[(burn_in+1):num_mcmc,] %>% as.mcmc() %>% summary()
-beta_samples[(burn_in+1):num_mcmc,] %>% as.mcmc() %>% HPDinterval()
-beta_samples[(burn_in+1):num_mcmc,] %>% as.mcmc() %>% effectiveSize()
 
-sqrt(sigmasq_samples[(burn_in+1):num_mcmc]) %>% as.mcmc() %>% summary()
+#beta_samples[(burn_in+1):num_mcmc,2] %>% mean()
+#beta_samples[(burn_in+1):num_mcmc,2] %>% hist()
+#beta_samples[(burn_in+1):num_mcmc,1] %>% hist()
 
-tibble(beta0 = beta_samples[(burn_in+1):num_mcmc,1], iteration = 1:(num_mcmc - burn_in)) %>% 
-  ggplot(aes(y = beta0,iteration)) + geom_line()
+#beta_samples[(burn_in+1):num_mcmc,] %>% as.mcmc() %>% HPDinterval()
+#beta_samples[(burn_in+1):num_mcmc,] %>% as.mcmc() %>% effectiveSize()
+
+#tibble(beta0 = beta_samples[(burn_in+1):num_mcmc,1], iteration = 1:(num_mcmc - burn_in)) %>% 
+#  ggplot(aes(y = beta0,iteration)) + geom_line()
 
