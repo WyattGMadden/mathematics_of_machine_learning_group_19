94f52116f0c25e0bcff4328fe9ca4d1d62dd43a1
dncrwlye <dancrowley.e@gmail.com>
2020-03-08 10:06:24 -0600

    fixed the data concatenation again

--- labs/lab_5/make_cloud.py
+++ labs/lab_5/make_cloud.py
@@ -21,26 +21,18 @@ def make_cloud():
     X0 = 6.5* np.random.uniform(low=0.0, high=1.0, size=500)
     X1 = 6.5* np.random.uniform(low=0.0, high=1.0, size=500)
     
-    X0 = np.stack((0.5*X0, X0))
-    X0 = np.stack((np.sin(X0[0]),  np.sin(X0[1])))
+    X0 = np.concatenate((np.sin(0.5*X0), np.sin(X0)))
+    X1 = np.concatenate((np.sin(0.5*X1), np.sin(X1)))
 
-    X1 = np.stack((0.5*X1, X1))
-    X1 = np.stack((np.sin(X1[0]),  np.sin(X1[1])))
-
-    X = np.vstack((X0, X1))
+    X = np.stack((X0, X1))
     
     a = np.array(np.repeat(1, 500))
-    b = np.array(np.repeat(1, 500))
+    b = np.array(-1*np.repeat(1, 500))
     t = np.concatenate((a,b),axis=0)
-    
-
     
     return(X, t)
 
-
 data = make_cloud()
-
-plot(data[0][1])
 
 import matplotlib.pyplot as plt
 
