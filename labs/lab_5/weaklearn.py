2be53d8d6188f179c724f4bd8b0fd6db916eabfc
dncrwlye <dancrowley.e@gmail.com>
2020-03-08 17:15:54 -0600

    should do it

--- labs/lab_5/weaklearn.py
+++ labs/lab_5/weaklearn.py
@@ -8,42 +8,25 @@
 # Outputs
 #  params - Parameters for the weak trained model (column vector).
 
-<<<<<<< HEAD
-def weaklearn(X, t, v[optional]):
-=======
-#data = make_cloud()
 
-
-import math
-
-t = data[1]
-X = data[0]
 
 def weaklearn(X, t, v):
->>>>>>> dbbd9ccef27caeb628d5b20747d9bbcc6b136985
     
-    X0b = X.transpose()[t==1];
-    X1b = X.transpose()[t==-1];
+    X0b = X.transpose()[t==1]; #DATA IN CLASS 1
+    X1b = X.transpose()[t==-1]; #DATA IN CLASS -1
 
     #if nargin == 2 ##nargin is matlab code for "number of arguments"
     #    W0 = ones(size(X0,2),1);
     #    W1 = ones(size(X1,2),1);
     #else
     
-<<<<<<< HEAD
     if v is None:
-        W0 = np.oones(np.size(X0,2),1);
-        W1 = ones(size(X1,2),1);
-    else
+        W0 = np.ones(X0b.shape[0]);
+        W1 = np.ones(X1b.shape[0]);
+    else:
         W0 = v(t==+1);
         W1 = v(t==-1);
-    end
-=======
-        W0 = t[t==+1];
-        W1 = t[t==-1];
-    #end
->>>>>>> dbbd9ccef27caeb628d5b20747d9bbcc6b136985
-    
+      
     best_d = 1;
     best_x = 0;
     best_err = math.inf; #update python code to use inf value
@@ -52,50 +35,52 @@ def weaklearn(X, t, v):
     
     X = np.concatenate((X0b, X1b), axis =0)
 
-    W = np.concatenate((W0, W1),axis=0); #i can't tell if this is what zosso did
+    W = np.concatenate((-W0, W1),axis=0); #i can't tell if this is what zosso did
     
-     #fixed up to here
+    #fixed up to here
+    #for d = 1:size(X0,1)  #If you call size(A, 1), size will return a scalar equal to the number of rows in A.
+    #so here we are going through the 500 iterations
+    #i would have guessed we loop through all obs...not just the ones in group 1
 
-    for d in range(1,X0.shape[1]):
-            # i think this is doing one column at a time?
-            #for d in range(1,X0.shape[1]):
-            # matlab indexes by row first 
+    #okay but in this case we are supposed to grab the number 2
+
+    for d in range(1,X0b.shape[1]):
+         
+        #grab the dth row 
+        #[~,IX] = sort(X(d,:)); ~ means logical "not" in matlab 
         
-        #i would have guessed we loop through by obs
-        np.sort(X[d,:])
-        [~,IX] = sort(X(d,:));
+        IX = np.argsort(X[:,d]);
         
-        err = cumsum(W(IX));
-        [min_cum,min_k] = min(err);
-        best_01 = sum(W0) + min_cum;
-        best_01_x = X(d,IX(min_k));
+        err= np.cumsum(W[IX]) #err = cumsum(W(IX));
+
+        min_cum = np.min(err);
+        min_k   = np.argmin(err);
+        best_01 = sum(W0)  + np.min(err); # + min_cum
+        best_01_x = X[IX[min_k],d];
         
-        err = cumsum(-W(IX));
-        [min_cum,min_k] = min(err);
+        err = np.cumsum(-W[IX]);        
+        min_cum = np.min(err);
+        min_k   = np.argmin(err);
+        
         best_10 = sum(W1) + min_cum;
-        best_10_x = X(d,IX(min_k));
+        best_10_x = X[IX[min_k],d];
        
-        if best_01 < best_err
+        if best_01 < best_err:
             best_d = d;
             best_x = best_01_x;
             best_err = best_01;
             is_01 = 1;
-        end
         
-        if best_10 < best_err
+        if best_10 < best_err:
             best_d = d;
             best_x = best_10_x;
             best_err = best_10;
             is_01 = 0;
-        end
-    end
-    
-    beta = zeros(size(X0,1),1);
-    beta(best_d) = 1;
-    params = [beta;-best_x];
-    if is_01
+            
+    beta = np.zeros(X0b.shape[1]);
+    beta[best_d] = 1;
+    params = [beta, -best_x];
+    if is_01 == 0:
         params = -params;
-    end
-end
 
-return params
+    return(params)
