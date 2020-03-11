6338f725119d10cd5b2d541ddb8d78cc0c28add6
Wyatt Madden <wyattmaddensonoma@gmail.com>
2020-03-10 18:25:26 -0600

    soemthing wrong with boostlean, why frac pred < 0.5?

--- labs/lab_5/boostlearn.py
+++ labs/lab_5/boostlearn.py
@@ -9,9 +9,23 @@
 #  alpha - A vector of weights used to combine the results of the
 #    M weak learners.
 
-def boostlean(X, t, M):
-    
-  
-   
-   return params, alpha
-end
+import numpy as np
+from weakeval import *
+from weaklearn import *
+
+def boostlearn(X, t, M):
+    weights = np.empty((M + 1, len(t)))
+    weights[0, ] = np.repeat(1/np.size(t), np.size(t))
+    params = np.empty((M, np.shape(X)[0] + 1))
+
+    for i in range(M):
+        params[i,] = weaklearn(X = X, t = t, v = weights[i, ])
+        preds = weakeval(X = X, params = params[i, ])
+        pred_correct = (preds == t)
+        frac_pred_correct = np.sum(pred_correct) / len(pred_correct)
+        epsilon = np.sum(weights[i, pred_correct]) / np.sum(weights[i,])
+        alpha = np.log((1 - epsilon) / epsilon)
+        weights[i + 1, pred_correct] = weights[i, pred_correct] * (np.exp(alpha))
+
+    weights = weights[0:M,]
+    return params, weights
