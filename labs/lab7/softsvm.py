022a90ca12905e0275d50a9647d25c9c58235a80
dncrwlye <dancrowley.e@gmail.com>
2020-03-31 13:34:55 -0600

    oooo boy

--- labs/lab7/softsvm.py
+++ labs/lab7/softsvm.py
@@ -30,27 +30,15 @@ Created on Mon Mar 30 12:33:47 2020
 # classify data using sign( X'*w + b )
 import scipy.io as scipy_io
 import numpy as np
+import quadprog
+import cvxopt
+from cvxopt import matrix, solvers # run in terminal to install: conda update -n base -c defaults conda
+#https://cvxopt.org/install/index.html
+#https://scaron.info/blog/quadratic-programming-in-python.html
 mat = scipy_io.loadmat('//Users/dancrowley/documents/machine_learning_zosso/mathematics_of_machine_learning_group_19/labs/lab7/cbcl1.mat')
 
 
-
-import quadprog
-
-def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None):
-    qp_G = .5 * (P + P.T)   # make sure P is symmetric
-    qp_a = -q
-    if A is not None:
-        qp_C = -np.vstack([A, G]).T
-        qp_b = -np.hstack([b, h])
-        meq = A.shape[0]
-    else:  # no equality constraint
-        qp_C = -G.T
-        qp_b = -h
-        meq = 0
-    return quadprog.solve_qp(qp_G, qp_a, qp_C, qp_b, meq)[0]
-
-
-#https://scaron.info/blog/quadratic-programming-in-python.html
+#sol=solvers.lp(c,A,b)
 
 X = mat["X"]
 l = mat["L"]
@@ -59,26 +47,33 @@ def softsvm(X, l, gamma):
     D,N = X.shape
 
     x = np.repeat(1, N + D + 1) #should it be 1? i honestly dont know
-    G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0.00001, N), np.repeat(1, D), np.repeat(0.00001,1)), axis = 0)
-    #G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0, N), np.repeat(1, D), np.repeat(0,1)), axis = 0)
+    #G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0.00001, N), np.repeat(1, D), np.repeat(0.00001,1)), axis = 0)
+    P = np.identity(n=N+D+1) * np.concatenate((np.repeat(0, N), np.repeat(1, D), np.repeat(0,1)), axis = 0)
+    q = np.concatenate((np.repeat(1, N), np.repeat(0, D + 1)))
+    G =np.concatenate((-1*np.identity(N), np.transpose(np.dot(np.identity(N) * l, np.transpose(X))), np.transpose(-1*l)))
+    G= np.transpose(G)
+    h = np.repeat(-1, G.shape[1]) 
+    A = np.identity(n = N + D + 1)
+    b= np.repeat(1, N + D + 1)
+    
+    P = matrix(P.astype('float'))
+    q = matrix(q.astype('float'))
+    G = matrix(G.astype('float'))
+    h = matrix(h.astype('float'))
+    A = matrix(A.astype('float'))
+    b = matrix(b.astype('float'))
 
-    a = np.repeat(0, N+D+1)
+    sol = cvxopt.solvers.qp(P,q,G, h, A, b)
+    #http://cvxopt.org/userguide/coneprog.html#quadratic-programming
     
-    c =np.concatenate((-1*np.identity(N), np.transpose(np.dot(np.identity(N) * l, np.transpose(X))), np.transpose(-1*l)))
-    
-    b = np.repeat(-1, np.dot(np.transpose(c), x).shape)
-    
-    #figuring this out was such a pain: 
-    G = G.astype(np.double)
-    c = c.astype(np.double)
-    a= a.astype(np.double)
-    b= b.astype(np.double)
-    meq = 1
+  
     #construct H, f, A, b, and lb
     #quadprog.solve_qp()
         #min 1/2 (x.T G X + ax)
         #st c x <= b
-    quadprog.solve_qp(G, a, c, b, meq)
+    sol=solvers.qp(Q, p, h)
+    
+    #sol = quadprog.solve_qp(G, a, c, b, meq)
 
 # distribute components of x into w, b, and xi:
 
