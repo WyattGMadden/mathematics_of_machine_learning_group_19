c7b796811ec9d7daa6b0546d56ca51178cb87583
dncrwlye <dancrowley.e@gmail.com>
2020-03-30 19:25:06 -0600

    dealing with the issue of PD/PSD for matrix G still...

--- labs/lab7/softsvm.py
+++ labs/lab7/softsvm.py
@@ -59,7 +59,9 @@ def softsvm(X, l, gamma):
     D,N = X.shape
 
     x = np.repeat(1, N + D + 1) #should it be 1? i honestly dont know
-    G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0, N), np.repeat(1, D), np.repeat(0,1)), axis = 0)
+    G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0.00001, N), np.repeat(1, D), np.repeat(0.00001,1)), axis = 0)
+    #G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0, N), np.repeat(1, D), np.repeat(0,1)), axis = 0)
+
     a = np.repeat(0, N+D+1)
     
     c =np.concatenate((-1*np.identity(N), np.transpose(np.dot(np.identity(N) * l, np.transpose(X))), np.transpose(-1*l)))
