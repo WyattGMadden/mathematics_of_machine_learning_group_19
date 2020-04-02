fa1c0bae9284ceebb6b2f78914812dc11d0f562f
dncrwlye <dancrowley.e@gmail.com>
2020-04-02 17:08:25 -0600

    changes

--- labs/lab7/softsvm.py
+++ labs/lab7/softsvm.py
@@ -32,6 +32,8 @@ import scipy.io as scipy_io
 from scipy import sparse 
 #WYATTT CHECK OUT THE SPARSE STUFF< I HAVEN"T INCLUDED IT YET BUT SHOULD
 import numpy as np
+import math
+
 import quadprog
 import cvxopt
 from cvxopt import matrix, solvers # run in terminal to install: conda update -n base -c defaults conda
@@ -49,31 +51,41 @@ def softsvm(X, l, gamma):
     D,N = X.shape
 
     x = np.repeat(1, N + D + 1) #should it be 1? i honestly dont know
-    #G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0.00001, N), np.repeat(1, D), np.repeat(0.00001,1)), axis = 0)
+    G = np.identity(n=N+D+1) * np.concatenate((np.repeat(0.00001, N), np.repeat(1, D), np.repeat(0.00001,1)), axis = 0)
+    
     P = np.identity(n=N+D+1) * np.concatenate((np.repeat(0, N), np.repeat(1, D), np.repeat(0,1)), axis = 0)
     q = np.concatenate((np.repeat(1, N), np.repeat(0, D + 1)))
-    G =np.concatenate((-1*np.identity(N), np.transpose(np.dot(np.identity(N) * l, np.transpose(X))), np.transpose(-1*l)))
+    
+    I_n = -1*np.identity(N)
+    LdotX = -1*np.dot(np.identity(N) * l, np.transpose(X))
+    lil_l = -1*l
+    
+    #now create the bottom part of "G", the infinity section
+    
+    G_bottom = -1*np.identity(n=N+D+1)
+    G = np.concatenate((I_n, np.transpose(LdotX), np.transpose(lil_l)))
+    #G =np.concatenate((-1*np.identity(N), np.transpose(np.dot(np.identity(N) * l, np.transpose(X))), np.transpose(-1*l)))
     G= np.transpose(G)
-    h = np.repeat(-1, G.shape[1]) 
+    
+    Gstack = np.concatenate((G, G_bottom))
+    h = np.concatenate((np.repeat(-1, G.shape[0]), np.zeros(N,), -1*math.inf*np.ones(D+1)))
+     
     A = np.identity(n = N + D + 1)
-    b= np.repeat(1, N + D + 1)
+    b= np.repeat(1, N + D +1)
     
     P = matrix(P.astype('float'))
     q = matrix(q.astype('float'))
-    G = matrix(G.astype('float'))
+    Gstack = matrix(Gstack.astype('float'))
     h = matrix(h.astype('float'))
     A = matrix(A.astype('float'))
     b = matrix(b.astype('float'))
 
-    sol = cvxopt.solvers.qp(P,q,G, h, A, b)
+    sol = cvxopt.solvers.qp(P,q,Gstack, h)
     #http://cvxopt.org/userguide/coneprog.html#quadratic-programming
-    
-  
-    #construct H, f, A, b, and lb
     #quadprog.solve_qp()
-        #min 1/2 (x.T G X + ax)
-        #st c x <= b
-    sol=solvers.qp(Q, p, h)
+        #min 1/2 (x.T P X + q.T x)
+        #st G x <= h
+        #st A x =  b
     
     #sol = quadprog.solve_qp(G, a, c, b, meq)
 
