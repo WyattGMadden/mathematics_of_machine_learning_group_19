0a0188044b3da6b5a26744a9d4e0b41dd7f26693
dncrwlye <dancrowley.e@gmail.com>
2020-04-03 13:37:39 -0600

    hheeeyyy

--- labs/lab7/svm_try_2.py
+++ labs/lab7/svm_try_2.py
@@ -8,34 +8,36 @@ Created on Fri Apr  3 10:33:58 2020
 
 import scipy.io as scipy_io
 from scipy import sparse 
-#WYATTT CHECK OUT THE SPARSE STUFF< I HAVEN"T INCLUDED IT YET BUT SHOULD
 import numpy as np
 import math
-import svmcmpl
 import quadprog
 import cvxopt
 from cvxopt import matrix, solvers 
-
+import cupyx
 
 mat = scipy_io.loadmat('//Users/dancrowley/documents/machine_learning_zosso/mathematics_of_machine_learning_group_19/labs/lab7/cbcl1.mat')
+X = mat["X"]
+
+D,N = X.shape
 
 #sol=solvers.lp(c,A,b)
 
 X = mat["X"]
 X = np.transpose(X)
 d = mat["L"]
+#so there are 6977 observations
+#D dimensions to each observation
+
 gamma = 2.0
 
 X = matrix(X.astype('float'))
 d = matrix(d.astype('float'))
-
-softmargin(X, d, gamma, kernel = 'linear', sigma = 1.0, degree = 1, theta = 1.0)
-
-
-D,N = X.shape
+width = 20
+sol = softmargin(X, d, gamma, kernel = 'linear', sigma = 1.0, theta = 1.0)
+#sol = softmargin_appr(X, d, gamma, width, kernel = 'linear', sigma = 1.0)
 
 
-Q = np.dot(np.transpose(X), X)
+#667 support vectors
+l  = list(sol['z'])
+print(sol['z'])
 
-d = l
-(np.identity(N) * l
